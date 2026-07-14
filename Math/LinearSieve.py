fbab2700a92dfab857e0ed334b408c2cf0e57e0e
Jonny Chang <26changj@gmail.com>
2026-07-14 16:22:19 -0400

    Added sieves

---
+++ Math/LinearSieve.py
@@ -0,0 +1,15 @@
+#Implements the linear Sieve of Eratosthenes for finding all primes less than or equal to n
+#Note: O(n) but generally slower than the normal sieve
+def LinearSieve(n: int):
+    composite = [False] * (n + 1)
+    prime = []
+    for i in range(2, n + 1):
+        if not composite[i]:
+            prime.append(i)
+        for j in range(len(prime)):
+            if i * prime[j] > n:
+                break
+            composite[i * prime[j]] = True
+            if not i % prime[j]:
+                break
+    return prime, composite
---
+++ Math/Sieve.py
@@ -0,0 +1,12 @@
+import math
+
+#Implements the Sieve of Eratosthenes for finding all primes less than or equal to n
+#Note: O(nlog(log(n))) but generally faster than linear sieve
+def Sieve(n: int):
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False  #0 and 1 aren't prime
+    for i in range(2, math.isqrt(n) + 1):
+        if prime[i]:
+            for j in range(2 * i, n + 1, i):
+                prime[j] = False
+    return prime
