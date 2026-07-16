f79b2879c3a1f1f10aebba3bf448d06b9bd63276
Jonny Chang <26changj@gmail.com>
2026-07-15 22:06:33 -0400

    Added implementation of the SPFA algorithm

---
+++ Graphs/SPFA.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import List, Tuple
+
+#Implements the shortest path faster algorithm that can deal with negative edge weights and find a negative cycle
+def SPFA(adj: List[List[Tuple[int, int]]], start: int) -> bool | List[int | float]:
+    q = deque([start])
+    dist = [float('inf')] * len(adj) #holds the min distance from start to any node
+    inqueue = [False] * len(adj) #prevents adding a node that is already in the queue
+    inqueue[start] = True
+    dist[start] = 0
+    count = [0] * len(adj) #counts the number of times the shortest distance to a node is updated
+
+    while q:
+        vertex = q.pop()
+        inqueue[vertex] = False
+        for edge in adj[vertex]:
+            if dist[vertex] + edge[1] < dist[edge[0]]:
+                dist[edge[0]] = dist[vertex] + edge[1]
+
+                if not inqueue[edge[0]]:
+                    inqueue[edge[0]] = True
+                    q.append(edge[0])
+                    count[edge[0]] += 1
+                    if count[edge[0]] > len(adj):
+                        return False #negative cycle
+    return dist
