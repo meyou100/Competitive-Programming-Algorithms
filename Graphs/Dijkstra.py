7ca56f7a7bb037000c99b34ed5b76b89116ac0ae
Jonny Chang <26changj@gmail.com>
2026-07-22 13:11:53 -0400

    Added the get_path function to find the shortest path between the start and end nodes

--- Graphs/Dijkstra.py
+++ Graphs/Dijkstra.py
@@ -3,12 +3,16 @@ import heapq
 
 #Implements Dijkstra's algorithm for finding the shortest path between a starting node to all other nodes
 #Fails if there are negative weights
-def Dijkstra(adj: List[List[Tuple[int, int] | List[int]]], start: int) -> List[int | float]:  #adjacency list, and start node
+#If provided with the parameter p=True, it will return a parent array representing the direct parent of a node in the shortest path
+def Dijkstra(adj: List[List[Tuple[int, int] | List[int]]], start: int, p: bool=False) -> List[int | float] | Tuple[List[int | float], List[int]]:  #adjacency list, and start node
     n = len(adj)
+    if p: #if the parent array is requested
+        parent = [-1] * n
     dist = [float('inf')] * n
     processed = [False] * n
     dist[start] = 0
     q = [(0, start)]  #min heap
+
     while q:
         z, cur = heapq.heappop(q)  #weight and current node
         if processed[cur]:
@@ -18,4 +22,19 @@ def Dijkstra(adj: List[List[Tuple[int, int] | List[int]]], start: int) -> List[i
             if dist[cur] + w < dist[nxt]:
                 dist[nxt] = dist[cur] + w
                 heapq.heappush(q, (dist[nxt], nxt))
+                if p:
+                    parent[nxt] = cur
+
+    if p:
+        return dist, parent
     return dist
+
+#Builds the shortest path from the start node to the given end node
+def get_path(parent: List[int], end: int) -> List[int]:
+    path = []
+    while parent[end] != -1:
+        path.append(end)
+        end = parent[end]
+    path.append(end) #append the start node
+    path.reverse() #the path was built backwards
+    return path
