620bf60c58068446f41d34bdb7dc68b39aff2e9e
Jonny Chang <26changj@gmail.com>
2026-07-23 16:36:27 -0400

    Made the comments in algorithm_L clearer

--- Random Algorithms/Reservoir Sampling.py
+++ Random Algorithms/Reservoir Sampling.py
@@ -27,8 +27,8 @@ def algorithm_R(stream: List[Any], k: int, queries: List[int]) -> List[Any]:
 
 #The optimal algorithm for reservoir sampling
 def algorithm_L(stream: List[Any], k: int, queries: List[int]) -> List[Any]:
-    #queries 0 means query the reservoir after the 0th index of stream is processed
-    #Makes sure the random function doesn't produce 0
+    #queries 0 means query the reservoir after the 0th element in stream is processed
+    #Makes the random.random() work on the interval (0, 1)
     def open_random() -> float:
         x = random.random()
         while not x:
@@ -37,29 +37,21 @@ def algorithm_L(stream: List[Any], k: int, queries: List[int]) -> List[Any]:
 
     result = [None] * len(queries)
     reservoir = [None] * k
-    i = 0
     qi = 0 #index in the queries list
-    while i < k: #initializing the reservoir
-        reservoir[i] = stream[i]
-        i += 1
-    i -= 1 #i must equal k - 1 for the while loop to work
     while qi < len(queries) and queries[qi] + 1 < k: #answering the queries that happen before the reservoir is fully initialized
         qi += 1
 
+    for i in range(k): #initializing the reservoir
+        reservoir[i] = stream[i]
+
     _max = open_random() ** (1/k)
     while i < len(stream):
-        i += int(math.log(open_random()) / math.log(1 - _max)) + 1
-        while qi < len(queries) and queries[qi] < i:
+        i += int(math.log(open_random()) / math.log(1 - _max)) + 1 #geometric distribution
+        while qi < len(queries) and queries[qi] < i: #answering previous queries
             result[qi] = reservoir[:]
             qi += 1
-        if i < len(stream):
+        if i < len(stream): #updating the reservoir and max variable
             reservoir[random.randrange(k)] = stream[i]
             _max *= open_random() ** (1 / k)
 
-        print(i, reservoir, qi)
     return result
-print(algorithm_L(list(range(6)), 5, queries=[0,1,2,3,4,5]))
-
-
-
-print(algorithm_R(list(range(1000)), k=5, queries=[0,1,2,3,4,5,999]))
