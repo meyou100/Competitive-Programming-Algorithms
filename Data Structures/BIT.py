0b20aed038bb7332b461a55a4039f5769915c81c
Jonny Chang <26changj@gmail.com>
2026-09-03 15:02:01 -0400

    Generalized the implementation of BIT

--- Data Structures/BIT.py
+++ Data Structures/BIT.py
@@ -1,27 +1,34 @@
-from typing import List
+import operator
+from typing import List, Callable
 
 class BIT:
-    def __init__(self, x: List[int]) -> None:
+    def __init__(self, x: List[int], f: Callable[[int, int], int] = operator.add, g: Callable[[int, int], int] = operator.sub) -> None:
         """transform list into BIT"""
         self.bit = x[:]
+        self.f = f #group operation
+        self.g = g #inverse operation
         for i in range(len(x)):
-            j = i | (i + 1)
+            j = i | (i + 1) #gets the parent of element i
             if j < len(x):
-                self.bit[j] += self.bit[i]
+                self.bit[j] = self.f(self.bit[j], self.bit[i]) #updates the parent with element i
 
     def update(self, idx: int, x: int) -> None:
-        """updates bit[idx] += x"""
+        """updates the element at idx by x"""
         while idx < len(self.bit):
-            self.bit[idx] += x
-            idx |= idx + 1
+            self.bit[idx] = self.f(self.bit[idx], x)
+            idx |= idx + 1 #gets the parent of element i
 
-    def query(self, end: int) -> int:
-        """calc sum(bit[:end])"""
+    def _prefix(self, end: int) -> int:
+        """calc f on the range [0, end)"""
         x = 0
         while end:
-            x += self.bit[end - 1]
-            end &= end - 1
+            x = self.f(x, self.bit[end - 1])
+            end &= end - 1 #gets the next block for calculating the query
         return x
+
+    def query(self, start: int, end: int) -> int:
+        """calc f on the range [start, end)"""
+        return self.g(self._prefix(end), self._prefix(start))
 
     def findkth(self, k: int) -> int:
         """Find largest idx such that sum(bit[:idx]) <= k"""
@@ -30,5 +37,5 @@ class BIT:
             right_idx = idx + (1 << d)
             if right_idx < len(self.bit) and k >= self.bit[right_idx]:
                 idx = right_idx
-                k -= self.bit[idx]
+                k = self.g(k, self.bit[idx])
         return idx + 1
