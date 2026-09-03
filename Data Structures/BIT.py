96873cf0d7c8344ebba65c10cf309d7e8a5716c9
Jonny Chang <26changj@gmail.com>
2026-09-03 17:20:03 -0400

    Changed some code relating to findkth to make its usage more clear

--- Data Structures/BIT.py
+++ Data Structures/BIT.py
@@ -32,11 +32,11 @@ class BIT:
         return self.g(self._prefix(end), self._prefix(start))
 
     def findkth(self, k: int) -> int:
-        """Find largest idx such that sum(bit[:idx]) <= k"""
+        """Find largest idx such that sum(bit[:idx]) <= k. the list must have no negative numbers and f = +"""
         idx = -1
         for d in reversed(range(len(self.bit).bit_length())):
             right_idx = idx + (1 << d)
             if right_idx < len(self.bit) and k >= self.bit[right_idx]:
                 idx = right_idx
-                k = self.g(k, self.bit[idx])
+                k -= self.bit[idx]
         return idx + 1
