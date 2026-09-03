7912fd27043048cdab597cdc75b34e8a1f670026
Jonny Chang <26changj@gmail.com>
2026-09-03 17:09:08 -0400

    Removed the findkth method from BIT Range Updates since it was not applicable for the class

--- Data Structures/BIT Range Updates.py
+++ Data Structures/BIT Range Updates.py
@@ -32,13 +32,3 @@ class RangeUpdateBIT:
             x = self.f(x, self.bit[idx - 1])
             idx &= idx - 1 #gets the next block for calculating the query
         return x
-
-    def findkth(self, k: int) -> int:
-        """Find largest idx such that sum(bit[:idx]) <= k"""
-        idx = -1
-        for d in reversed(range(len(self.bit).bit_length())):
-            right_idx = idx + (1 << d)
-            if right_idx < len(self.bit) and k >= self.bit[right_idx]:
-                idx = right_idx
-                k = self.g(k, self.bit[idx])
-        return idx + 1
