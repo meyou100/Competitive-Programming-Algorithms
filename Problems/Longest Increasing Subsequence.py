3e4a09bf0b11a8e315612545964e7a251c3ed9c7
Jonathan Chang <26changj@gmail.com>
2026-09-20 13:19:28 -0400

    Created more variations of the LIS algorithm

--- Problems/Longest Increasing Subsequence.py
+++ Problems/Longest Increasing Subsequence.py
@@ -16,7 +16,7 @@ def LIS(arr: List[int]) -> int:
 
 
 def LIS_dp(arr: List[int]) -> List[int]:
-    """Returns the length of the longest strictly increasing subsequence of arr"""
+    """Returns the length of the longest strictly increasing subsequence of arr[:i + 1]"""
     seq = []
     prev = [-1] * len(arr) #previous element to element i in the subseq
 
@@ -36,3 +36,47 @@ def LIS_dp(arr: List[int]) -> List[int]:
             prev[i] = prev[prev[i]] + 1
 
     return prev
+
+
+def LIS_arr(arr: List[int]) -> List[int]:
+    """Returns a longest strictly increasing subsequence of arr"""
+    seq = []
+    prev = [-1] * len(arr) #previous element to element i in the subseq
+
+    for i in range(len(arr)):
+        t = bisect.bisect_left(seq, (arr[i], 0))
+        if t == len(seq):
+            seq.append((arr[i], i))
+        else:
+            seq[t] = (arr[i], i)
+        if t > 0:
+            prev[i] = seq[t - 1][1]
+
+    out = []
+    ind = seq[-1][1]
+    while ind != -1:
+        out.append(arr[ind])
+        ind = prev[ind]
+    out.reverse()
+    return out
+
+
+def LIS_count(arr: List[int]) -> int:
+    """Returns the number of longest strictly increasing subsequences of arr"""
+    class BIT:
+        def __init__(self, n: int) -> None:
+            self.bit = [[0, 0] for _ in range(n)]
+
+        def update(self, idx: int, x: int) -> None:
+            """updates the element at idx by x"""
+            while idx < len(self.bit):
+                self.bit[idx] = max(self.bit[idx], x)
+                idx |= idx + 1  # gets the parent of element i
+
+        def query(self, end: int) -> int:
+            """calc f on the range [0, end)"""
+            x = [float('-inf'), 0]
+            while end:
+                x = max(x, self.bit[end - 1])
+                end &= end - 1  # gets the next block for calculating the query
+            return x
